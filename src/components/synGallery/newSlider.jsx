ae72b13c9a006a2ad3b1bc11cdb69105b54f4e8f
Yogesh Satale <yogesh.satale@syngenta.com>
2021-08-08 23:07:30 +0530

    after bug resolved autoplay and perfectworking

--- src/components/synGallery/newSlider.jsx
+++ src/components/synGallery/newSlider.jsx
@@ -1,139 +1,134 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable react-hooks/exhaustive-deps */
-// @flow
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import update from "immutability-helper";
 import { Slide } from "./slide";
 
 export const NewSlider = (props) => {
   const { media = [], autoPlay = false, autoDelay = 3000, range = 1 } = props;
-  const [activeSlides, setActiveSlides] = useState([]);
-  const [isShowImage, setIsShowImage] = useState(false);
-  const [zoomInImage, setZoomInImage] = useState("");
-  const [currentFirstSlide, setCurrentFirstSlide] = useState(0);
-  const [currentLastSlide, setCurrentLastSlide] = useState(range);
-
+  const [currentAllState, setCurrentAllState] = useState({
+    firstSlide: 0,
+    lastSlide: range,
+    activeSlides: media.slice(0, range),
+    zoomInImage: "",
+    isShowImage: false,
+  });
+  const { activeSlides, isShowImage, zoomInImage } = currentAllState;
   useEffect(() => {
-    setCurrentFirstSlide(0);
-    setCurrentLastSlide(range);
-    setActiveSlides(media.slice(0, range));
     if (autoPlay) {
-      let interval = setInterval(() => {
+      let interval = setTimeout(() => {
         next();
       }, autoDelay);
-      return () => clearInterval(interval);
+      return () => clearTimeout(interval);
     }
-    console.log("default", media.slice(0, range), autoPlay);
-  }, []);
-
+  }, [currentAllState]);
   const next = () => {
+    const { firstSlide, lastSlide } = currentAllState;
     let slides = [];
     let length = media.length;
-    let temp1 = currentFirstSlide + range; // 3 - 6
-    let temp2 = currentLastSlide + range; // 6 - 9
-    if (temp1 >= length) {
-      temp1 = temp1 % length;
+    let first = firstSlide + range; // 3 - 6
+    let last = lastSlide + range; // 6 - 9
+    if (first >= length) {
+      first = first % length;
     }
-    if (temp2 >= length) {
-      temp2 = temp2 % length;
-      let arr1 = media.slice(temp1, length); // getting last items
-      let arr2 = media.slice(0, temp2); // getting first items in serialy
+    if (last >= length) {
+      last = last % length;
+      let arr1 = media.slice(first, length); // getting first items
+      let arr2 = media.slice(0, last); // getting last items
       slides = arr1.concat(arr2);
     } else {
-      slides = media.slice(temp1, temp2);
+      slides = media.slice(first, last);
     }
-
-    setCurrentFirstSlide(() => {
-      return temp1;
-    }); // 3-
-    setCurrentLastSlide(() => {
-      return temp2;
-    }); // 6-
-    // setActiveSlides(slides);
-
-    setActiveSlides(() => {
-      return slides;
+    let temp = update(currentAllState, {
+      $merge: { firstSlide: first, lastSlide: last, activeSlides: slides },
     });
+    setCurrentAllState(temp);
   };
-
   const prev = () => {
     let slides = [];
     let length = media.length;
-    let temp1 = currentFirstSlide - range; //-3
-    let temp2 = currentLastSlide - range; // -6
+    let first = currentAllState.firstSlide - range; //-3
+    let last = currentAllState.lastSlide - range; // -6
 
-    if (temp2 < 0) {
-      temp2 = temp2 + length; // 3
+    if (last < 0) {
+      last = last + length; // 3
     }
-    if (temp1 < 0) {
-      temp1 = temp1 + length;
-      let arr1 = media.slice(temp1, length);
-      let arr2 = media.slice(0, temp2);
+    if (first < 0) {
+      first = first + length;
+      let arr1 = media.slice(first, length);
+      let arr2 = media.slice(0, last);
       slides = arr1.concat(arr2);
     } else {
-      slides = media.slice(temp1, temp2);
+      slides = media.slice(first, last);
     }
-    setCurrentFirstSlide(temp1);
-    setCurrentLastSlide(temp2);
-    setActiveSlides(slides);
+
+    let temp = update(currentAllState, {
+      $merge: { firstSlide: first, lastSlide: last, activeSlides: slides },
+    });
+    setCurrentAllState(temp);
   };
-
-  // useEffect(() => {
-  //   if (autoPlay) {
-  //     let interval = setInterval(() => {
-  //       next();
-  //     }, autoDelay);
-  //     return () => clearInterval(interval);
-  //   }
-  // }, [autoPlay]);
-
   const zoomIn = (val) => {
-    console.log("current slide", val);
-    setZoomInImage(val.src);
-    setIsShowImage(!isShowImage);
+    let { isShowImage } = currentAllState;
+    let temp = update(currentAllState, {
+      $merge: { zoomInImage: val.src, isShowImage: !isShowImage },
+    });
+    setCurrentAllState(temp);
   };
-
-  console.log("53=>", props, isShowImage, activeSlides);
-
   return (
     <>
-      <MainDiv data-testid="slidediv" slides={range}>
-        {activeSlides.map((slide) => (
-          <Slide
-            zoomIn={(val) => zoomIn(val)}
-            isShowImage={isShowImage}
-            zoomInImage={zoomInImage}
-            slide={slide}
-            key={slide.id}
-          />
-        ))}
-      </MainDiv>
+      {Object.keys(currentAllState).length > 0 && (
+        <MainDiv data-testid="slidediv" slides={range}>
+          {activeSlides.map((slide) => (
+            <Slide
+              zoomIn={(val) => zoomIn(val)}
+              isShowImage={isShowImage}
+              zoomInImage={zoomInImage}
+              slide={slide}
+              key={slide.id}
+            />
+          ))}
+        </MainDiv>
+      )}
       <div style={{ textAlign: "center" }}>
-        <a className="prev" data-testid="prevbutton" onClick={prev}>
+        <label className="prev" data-testid="prevbutton" onClick={prev}>
           &#10094;
-        </a>
-        <a className="next" data-testid="nextbutton" onClick={next}>
+        </label>
+        <label className="next" data-testid="nextbutton" onClick={next}>
           &#10095;
-        </a>
+        </label>
       </div>
     </>
   );
 };
-
 export const MainDiv = styled.div`
   background-color: #0e1318;
   width: 650px;
   height: 400px;
-
   overflow-x: hidden;
   overflow-y: hidden;
-
   display: grid;
   grid-template-columns: repeat(${(props) => props.slides}, auto);
   grid-gap: 0 40px;
-
   padding: 30px 30px 0;
-
   margin: 0 auto;
   border-radius: 10px;
+  /* @media screen and (max-width: 600px) and (orientation: landscape) {
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    transform: rotate(90deg);
+  } */
+  @media screen and (max-width: 460px) {
+    width: 100vw;
+    height: 100%;
+    transform: rotate(90deg);
+    overflow-x: hidden;
+    overflow-y: hidden;
+    display: grid;
+    grid-template-columns: repeat(${(props) => props.slides}, auto);
+    grid-gap: 0 40px;
+    padding: 30px 30px 0;
+    margin: 0 auto;
+    border-radius: 10px;
+  }
 `;
--- src/components/synGallery/slide.jsx
+++ src/components/synGallery/slide.jsx
@@ -1,32 +1,32 @@
-// @flow
 import React from "react";
 
 export const Slide = (props) => {
   const { slide, isShowImage, zoomInImage, zoomIn } = props;
-
   return (
     <>
-      {Object.keys(slide).length > 0 ? (
-        <div className="fade" key={slide.id}>
-          {slide.isVideo ? (
-            <video height={350} width={"100%"} controls>
-              <source src={slide.src} type="video/mp4" />
-            </video>
-          ) : (
-            <img
-              data-testid="imageid"
-              onClick={() => zoomIn(slide)}
-              height={350}
-              width={"100%"}
-              alt={slide.id}
-              src={slide.src}
-            />
-          )}
-          <h3 style={{ color: "white" }}>{slide.id}</h3>
-        </div>
-      ) : (
-        <div className="noData">No data</div>
-      )}
+      <div className="fade" key={slide.id}>
+        {Object.keys(slide).length > 0 ? (
+          <>
+            {slide.isVideo ? (
+              <video height={350} width={"100%"} controls>
+                <source src={slide.src} type="video/mp4" />
+              </video>
+            ) : (
+              <img
+                data-testid="imageid"
+                onClick={() => zoomIn(slide)}
+                height={350}
+                width={"100%"}
+                alt={slide.id}
+                src={slide.src}
+              />
+            )}
+            <h3 style={{ color: "white" }}>{slide.id}</h3>
+          </>
+        ) : (
+          <div className="noData">No data</div>
+        )}
+      </div>
       {isShowImage && (
         <div
           data-testid="zoominView"
